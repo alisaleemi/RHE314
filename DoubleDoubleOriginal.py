4605b027afeb0f0991d484f965e0feb26f17055f
alisaleemi <60361549+alisaleemi@users.noreply.github.com>
2020-02-17 09:29:21 -0600

    Add files via upload

---
+++ DoubleDoubleOriginal.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Modified Double Double (Part 2)
+
+# In[11]:
+
+
+def double(sequence):
+    total = []
+    for element in sequence:
+        total = total + [element*2]
+    return total
+
+
+# In[12]:
+
+
+double([1,2,3])
+
+
+# In[ ]:
+
+
+
+
